cd0e04acd272c9406bfff5ab3efecda4045ea2d9
AQodS <qoddri@gmail.com>
2025-06-25 23:47:59 +0700

    feat: restful API login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import register from "../controllers/RegisterController.js";
-import { validateRegister } from "../utils/validators/auth.js";
+import login from "../controllers/LoginController.js";
+import { validateRegister, validateLogin } from "../utils/validators/auth.js";
 
 const router = express.Router();
 
 router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
 
 export default router;
