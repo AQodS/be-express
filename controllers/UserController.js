d7bfa649a8079d088407d18fbf9ff517b58debf6
AQodS <qoddri@gmail.com>
2025-06-28 23:29:55 +0700

    feat: restful API insert user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { prisma } from "../prisma/client/index.js";
+import { validationResult } from "express-validator";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 
 const findUser = async (req, res) => {
   try {
@@ -27,4 +29,46 @@ const findUser = async (req, res) => {
   }
 };
 
-export { findUser };
+const createUser = async (req, res) => {
+
+  // Periksa hasil validasi
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // Jika ada error, kembalikan error ke pengguna
+    return res.status(422).json({
+      success: false,
+      message: "Validation error",
+      errors: errors.array(),
+    });
+  }
+
+  //hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  try {
+
+    //insert data
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "User created successfully",
+      data: user,
+    });
+
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export { findUser, createUser };
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import register from "../controllers/RegisterController.js";
 import login from "../controllers/LoginController.js";
 import { findUser } from "../controllers/UserController.js";
-import { validateRegister, validateLogin } from "../utils/validators/auth.js";
 import verifyToken from "../middlewares/auth.js";
+import { validateUser } from "../utils/validators/user.js";
+import { validateRegister, validateLogin } from "../utils/validators/auth.js";
 
 
 const router = express.Router();
 
 router.post("/register", validateRegister, register);
 router.post("/login", validateLogin, login);
-router.get("/users", verifyToken, findUser);
+router.get("/admin/users", verifyToken, findUser);
+router.post("/admin/users", verifyToken, validateUser, findUser);
 
 export default router;
