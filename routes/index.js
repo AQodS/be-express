9ead89061dcd8cfd4acb909b54baefef544b00eb
AQodS <qoddri@gmail.com>
2025-06-30 23:34:27 +0700

    feat: restful API update user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import login from "../controllers/LoginController.js";
 import verifyToken from "../middlewares/auth.js";
 import { validateUser } from "../utils/validators/user.js";
 import { validateRegister, validateLogin } from "../utils/validators/auth.js";
-import { createUser, findUser, findUserById } from "../controllers/UserController.js";
+import { createUser, findUser, findUserById, updateUser } from "../controllers/UserController.js";
 
 
 const router = express.Router();
@@ -14,5 +14,6 @@ router.post("/login", validateLogin, login);
 router.get("/admin/users", verifyToken, findUser);
 router.post("/admin/users", verifyToken, validateUser, createUser);
 router.get("/admin/users/:id", verifyToken, findUserById);
+router.put("/admin/users/:id", verifyToken, validateUser, updateUser);
 
 export default router;
